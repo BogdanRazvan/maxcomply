660288186a7efdbce72773945b0dc162203886a7
Razvan Bogdan <bogdrazvan@gmail.com>
2023-06-18 23:12:36 +0300

    Layout and sidebar update

--- frontend/src/components/EssentialLink.vue
+++ frontend/src/components/EssentialLink.vue
@@ -2,45 +2,41 @@
 	<q-item
 		clickable
 		tag="a"
-		target="_blank"
-		:href="link"
+		:to="{path: id.toString()}"
+		class="q-my-md"
 	>
-		<q-item-section
-			v-if="icon"
-			avatar
-		>
-			<q-icon :name="icon" />
-		</q-item-section>
-
 		<q-item-section>
-			<q-item-label>{{ title }}</q-item-label>
-			<q-item-label caption>
-				{{ caption }}
+			<q-item-label>{{ name }}</q-item-label>
+			<q-item-label
+				caption
+				class="sub-heading"
+			>
+				{{ subHeading }}
 			</q-item-label>
 		</q-item-section>
+		<q-item-section avatar>
+			<q-icon
+				name="chevron_right"
+				size="xl"
+			/>
+		</q-item-section>
 	</q-item>
+	<q-separator />
 </template>
 
 <script setup>
 defineProps({
-	title: {
+	name: {
 		type: String,
 		required: true,
 	},
-
-	caption: {
+	subHeading: {
 		type: String,
 		default: '',
 	},
-
-	link: {
-		type: String,
-		default: '#',
-	},
-
-	icon: {
-		type: String,
-		default: '',
+	id: {
+		type: Number,
+		default: 1,
 	},
 });
 </script>
---
+++ frontend/src/components/EssentialLinkFiltered.vue
@@ -0,0 +1,46 @@
+<template>
+	<div v-if="pStore.collection">
+		<q-input
+			debounce="1000"
+			outlined
+			placeholder=""
+			class="q-mx-sm"
+			:model-value="search"
+			@update:model-value="(value) => emit('update:search', value)"
+		>
+			<template #append>
+				<q-icon name="search" />
+			</template>
+		</q-input>
+		<essential-link
+			v-for="link in filteredCollection"
+			:key="link.id"
+			v-bind="link"
+		/>
+	</div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import EssentialLink from 'src/components/EssentialLink.vue';
+import { taskStore } from 'src/stores/taskStore.js';
+
+const pStore = taskStore();
+
+const props = defineProps({
+	search: {
+		type: String,
+		required: true,
+		default: '',
+	},
+});
+
+const emit = defineEmits(['update:search']);
+pStore.getCollection('http://localhost:3000/tasks/');
+
+const filteredCollection = computed(() => {
+	return !props.search ?
+		pStore?.collection?.data :
+		pStore?.collection?.data?.filter(item => item.name.toLowerCase().includes(props.search));
+});
+</script>
