50c5ef2749dd635bfc7e8a4a6b96fa4103aedb51
Razvan Bogdan <razvan.bogdan@nshift.com>
2023-06-25 19:37:16 +0300

    disable form after submit

--- frontend/src/pages/SingleComponentPage.vue
+++ frontend/src/pages/SingleComponentPage.vue
@@ -2,6 +2,9 @@
 <template>
 	<div>
 		<div v-if="pStore?.collection?.data && pStore?.item?.data && !pStore.loading">
+			<h4 class="text-h4 text-bold q-mb-md q-mt-sm">
+				{{ pStore?.item?.data[0].name }}
+			</h4>
 			<q-form
 				greedy
 				class="row q-col-gutter-xl"
@@ -11,11 +14,15 @@
 					:is="comp(field.type)"
 					v-for="(field, index) in pStore?.item?.data[0].fields"
 					:key="field.id"
+					:disable="pStore?.item?.data[0].status === 'completed'"
 					:class="`col-xs-${field.size.xs} col-sm-${field.size.sm} col-md-${field.size.md}
 		col-lg-${field.size.lg}`"
 					:index="index"
 				/>
-				<div class="fit text-right">
+				<div
+					v-if="pStore?.item?.data[0].status === 'open'"
+					class="fit text-right"
+				>
 					<q-btn
 						label="Submit"
 						type="submit"
@@ -50,14 +57,25 @@ pStore.getItem(`http://localhost:3000/tasks/${route.params.id}`);
 
 router.beforeEach((to) => {
 	pStore.getItem(`http://localhost:3000/tasks/${to.params.id}`);
+	pStore.$reset();
 });
 
 const capitalize = (text) => resolveComponent(text.charAt(0).toUpperCase() + text.slice(1).toLowerCase() + 'Component');
 const comp = (text) => defineAsyncComponent(() => import(`../components/${capitalize(text)}.vue`));
 
 const onSubmit = () => {
+	pStore.item.data[0].status = 'completed';
+	console.log({
+		...pStore.item.data[0],
+		status: 'completed',
+	});
 	pStore.setCollection(`http://localhost:3000/tasks/${route.params.id}`, {
-		fields: pStore.taskData.filter((item) => item),
+		data: [
+			{
+				...pStore.item.data[0],
+				status: 'completed',
+			}
+		],
 	});
 };
 </script>
